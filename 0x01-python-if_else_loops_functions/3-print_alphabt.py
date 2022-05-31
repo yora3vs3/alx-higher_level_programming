9fec3a96ceff4062f18c4ce99013e9f8d9caaf80
yora3vs3 <99631524+yora3vs3@users.noreply.github.com>
2022-05-31 16:09:39 +0300

    light

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# 4-print_hexa.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-"""Print numbers 0 to 98 in decimal and hexadecimal."""
-for number in range(0, 99):
-    print("{} = {}".format(number, hex(number)))
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
