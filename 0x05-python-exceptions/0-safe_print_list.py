394a0fefb57a9263020fed4e48ee69167bf412f3
yora3vs3 <mayora.brian@yahoo.com>
2022-06-21 04:46:10 -0700

    June 21, 2022 4:46 AM

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -20,4 +20,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print("")
     return (ret)
-    
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -20,4 +20,4 @@ def safe_print_list_integers(my_list=[], x=0):
             continue
     print("")
     return (ret)
-    
+   
