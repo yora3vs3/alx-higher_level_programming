ac3af327875eb6ec7e7964692a0408ea33b3d7c7
yora3vs3 <mayora.brian@yahoo.com>
2022-08-09 12:43:20 +0300

    main

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,3 @@
--- Prints the full description of the table first_table.
-SHOW CREATE TABLE `first_table`;
+-- Lists all rows of the table `first_table` from
+-- database `hbtn_0c_0` in MySQL Server.
+SELECT * FROM first_table;
